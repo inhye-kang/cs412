46a95182ef3c491098480f1be5cf33c0e83bbf0d
inhye <inhye@bu.edu>
2024-10-21 20:33:30 -0400

    Added views and template for mini_fb

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,7 +2,7 @@
 ## description: the app-specific URLS for the mini_fb application
 
 from django.urls import path
-from .views import ShowAllView, ShowProfileView, CreateProfileView, CreateStatusMessageView # our view class definition 
+from .views import ShowAllView, ShowProfileView, CreateProfileView, CreateStatusMessageView, UpdateProfileView, DeleteStatusMessageView, UpdateStatusMessageView # our view class definition 
 
 urlpatterns = [
     # map the URL (empty string) to the view
@@ -10,5 +10,9 @@ urlpatterns = [
     path('', ShowAllView.as_view(), name='show_all_profiles'), 
     path('profile/<int:pk>', ShowProfileView.as_view(), name='profile'),
     path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
-    path('profile/<int:pk>/create_status', CreateStatusMessageView.as_view(), name='create_status')
+    path('profile/<int:pk>/create_status', CreateStatusMessageView.as_view(), name='create_status'),
+    path('profile/<int:pk>/update', UpdateProfileView.as_view(), name='update_profile'),
+    path('status/<int:pk>/delete', DeleteStatusMessageView.as_view(), name='delete_status'),
+    path('status/<int:pk>/update', UpdateStatusMessageView.as_view(), name='update_status'),
+
 ]
